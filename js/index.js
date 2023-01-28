41884f7a408791996399c860dcd0b718c67fba19
balo-ohba <77915116+balo-ohba@users.noreply.github.com>
2023-01-28 16:25:39 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+let headerPositionY = 0
 window.addEventListener("scroll", function () {
     // スクロール量の取得
     let scrollValue = window.pageYOffset;
@@ -11,7 +12,7 @@ window.addEventListener("scroll", function () {
     // はみ出させる値（お好みで設定）
     let value = 300;
 
-    // 条件設定
+    //条件設定
     for (let a = 0; a < scrollEle.length; a++) {
         // 取得した要素のtop値の取得 + スクロール量
         let scrollTop = scrollEle[a].getBoundingClientRect().top + scrollValue;
@@ -19,4 +20,29 @@ window.addEventListener("scroll", function () {
             scrollEle[a].classList.add("js-fade");
         }
     }
+
+    operateHeader();
 });
+
+function operateHeader() {
+    //ヘッダー要素格納
+    let headerEle = document.querySelector("#header");
+    //ヘッダーを切り替えるしきい値 必要に応じて変更
+    let HEADTHRE = document.querySelector(".mainArea").clientHeight;
+    //スクロール上下でheader 切り替え
+    //過去のスクロール量と現在のスクロール量を比較
+    if (headerPositionY < window.pageYOffset) {
+        headerEle.classList.add("js-headerHide");
+    } else {
+        headerEle.classList.remove("js-headerHide");
+    }
+    //現在のスクロール量を保存
+    headerPositionY = window.pageYOffset;
+
+    //スクロール量によって背景色
+    if (HEADTHRE < window.pageYOffset) {
+        headerEle.classList.add("js-scroll");
+    } else {
+        headerEle.classList.remove("js-scroll");
+    }
+}
